5c4ebf9af94d1913a30930057841665baa05c470
laisMaas <lais_m_lopes@yahoo.com.br>
2020-10-30 12:12:50 -0300

    Adicionando informações de estudo do DOM -  Document Object Model

--- FrontEnd_TrilhaI/modulo2/ex2_operacoesAritmeticas.js
+++
@@ -1,9 +0,0 @@
-
-//Passando operações diretamente como parâmetro
-console.log("Operações Aritméticas")
-console.log(2 + 2)
-
-//Precedência de Operadores
-console.log(10 + 8 * 2)
-console.log((10 + 8) * 2)
-
