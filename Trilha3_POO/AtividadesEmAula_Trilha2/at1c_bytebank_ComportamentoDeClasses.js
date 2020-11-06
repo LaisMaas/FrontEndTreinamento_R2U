caf34b15990ecd0b37a49e65ac325671bc1545c8
laisMaas <lais_m_lopes@yahoo.com.br>
2020-11-05 23:54:26 -0300

    Atualizando atividade byteBank aula comportamento das classes

--- Trilha3_POO/AtividadesEmAula_Trilha2/at1c_bytebank_ComportamentoDeClasses.js
+++ Trilha3_POO/AtividadesEmAula_Trilha2/at1c_bytebank_ComportamentoDeClasses.js
@@ -1,47 +1,66 @@
-  
-class Cliente{
-    nome;
+
+//Abstraindo as necessidades para construção da classe na aplicação bytebank temos:
+/*
+nome
+CPF
+saldo
+agencia*/
+
+//Classe cliente
+class Cliente {
+    cliente;
     cpf;
+    rg;
 }
 
 class ContaCorrente{
     agencia;
-     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0)
-        {
-            return;
-        } 
-        this._saldo += valor;           
-    }
+    saldo;
 }
 
+const ContaCorrente1 = new ContaCorrente();
+ContaCorrente1.agencia = 1001;
+ContaCorrente1.saldo = 0;
+
+const ContaCorrente2 = new ContaCorrente();
+ContaCorrente1.agencia = 1001;
+ContaCorrente1.saldo = 0;
+
+const ContaCorrente3 = new ContaCorrente();
+ContaCorrente1.agencia = 1001;
+ContaCorrente1.saldo = 0;
+
+const ContaCorrente4 = new ContaCorrente();
+ContaCorrente1.agencia = 1001;
+ContaCorrente1.saldo = 0;
+
+
+//Construindo os objetos cliente 1,2,3,4 através da palavra reservada new
+//new Cliente(); -> Constroi o objeto cliente
+//const cliente1,2,3,4 -> aloca a construção deste cliente dentro das respectivas variáveis, para que estes possam ser instanciados 
 const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
-
 const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+const cliente3 = new Cliente();
+const cliente4 = new Cliente();
 
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
+//Instanciando os objetos da classe
+cliente1.nome = "Pedro Henrique";
+cliente1.cpf = 12345678911;
+cliente1.rg = 23456765;
 
-contaCorrenteRicardo.depositar(-100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+cliente2.nome = "Pietro";
+cliente2.cpf = 12345678912;
+cliente2.rg = 987654
 
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+cliente3.nome = "Manuella";
+cliente3.cpf= 12345678913;
 
-console.log(contaCorrenteRicardo);
+cliente4.nome = "Victória";
+cliente4.cpf = 12345678914;
+
+
+console.log(cliente1);
+console.log(cliente2);
+console.log(cliente3);
+console.log(cliente4);
