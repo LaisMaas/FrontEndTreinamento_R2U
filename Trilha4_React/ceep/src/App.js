80f0aa71af91d051a00ec2ededb1826e23cca9bf
laisMaas <lais_m_lopes@yahoo.com.br>
2020-11-13 13:55:08 -0300

    finalizacao projeto ceep

--- Trilha4_React/ceep/src/App.js
+++ Trilha4_React/ceep/src/App.js
@@ -1,20 +1,44 @@
-import React, { Component } from 'react';
-import ListaDeNotas from "./componentes/listaDeNotas";
-import FormularioCadastro from "./componentes/formularioCadastro";
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
-import "./assets/index.css";
+import './assets/index.css';
+class App extends Component {
 
+  constructor(){
+    super();
 
-class App extends Component{
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
+  deletarNota(index){
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index,1);
+    this.setState({notas:arrayNotas})
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro/>
-        <ListaDeNotas/>      
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas 
+        apagarNota={this.deletarNota.bind(this)}
+        notas={this.state.notas}/>
       </section>
     );
   }
 }
-//react  --> lib
-//React  --> ecossistema
+
+//new ListaDeNotas({notas:this.notas})
 export default App;
--- Trilha4_React/ceep/src/componentes/formularioCadastro/index.js
+++
@@ -1,3 +0,0 @@
-import FormularioCadastro from "./formularioCadastro";
-import formularioCadastro from "./formularioCadastro";
-export default FormularioCadastro;
